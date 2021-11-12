901d9dc4401073352abf47b388935c1bfd82d2b8
pedrohenrique <pedro.almeida@delage.com.br>
2021-11-11 22:35:14 -0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,21 @@
 import { Component } from '@angular/core';
 
+interface Currency {
+  value: string;
+  viewValue: string;
+}
+
+interface Select {
+  value: string;
+}
+
+type CurrencyResponse = {
+  info: {
+    rate: number;
+  };
+  result: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +23,53 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'currency-converter';
+
+  fromText = 0;
+  toText = 0;
+
+  isLoading = false
+
+  currentSelectedFrom = ''
+  currentSelectedTo = '';
+
+  currencies: Currency[] = [
+    {value: 'BRL', viewValue: 'Brazil'},
+    {value: 'USD', viewValue: 'EUA'},
+    {value: 'CAD', viewValue: 'Canada'},
+  ];
+
+  onChangeFromText(event: Event) {
+    const input = event.target as HTMLInputElement;
+
+    this.fromText = parseInt(input.value);
+
+    this.callCurrencyConversor();
+  }
+
+  onChangeFromSelect(event: Select) {
+    this.currentSelectedFrom = event.value;
+
+    this.callCurrencyConversor();
+  }
+
+  onChangeToSelect(event: Select) {
+    this.currentSelectedTo = event.value;
+
+    this.callCurrencyConversor();
+  }
+
+  async callCurrencyConversor() {
+    if (this.currentSelectedFrom.length > 0 &&
+      this.currentSelectedTo.length > 0 &&
+      this.fromText > 0) {
+        this.isLoading = true;
+
+        const url = `https://api.exchangerate.host/convert?from=${this.currentSelectedFrom}&to=${this.currentSelectedTo}&amount=${this.fromText}`;
+
+        const response: CurrencyResponse = await (await fetch(url)).json();
+
+        this.toText = response.result;
+        this.isLoading = false;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatCardModule} from '@angular/material/card';
+import { MatSelectModule} from '@angular/material/select';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatSelectModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
